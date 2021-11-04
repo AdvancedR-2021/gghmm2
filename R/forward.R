b94f404734d35fa70a558ff023914d0da9634da2
Esben <esbenskipper@outlook.com>
2021-11-04 12:15:07 +0100

    Needs fixing

--- R/forward.R
+++ R/forward.R
@@ -1,18 +1,31 @@
 #mangler generalisering af dpois
 
-forward <- function(delta,trans,X,param){
+forward <- function(HM,X){
+  if (!is.null(HM) ){
+    trans = HM$transmision
+    delta = HM$stationary_dist
+    param = HM$param
+    emisf = HM$emission_func
+  }
   m = dim(trans)[1]
   t = length(X)
   alpha_matrix = matrix(c(0),m,t)
   for (i in c(1:m)){
-    alpha_matrix [i,1] = delta[i] * dpois(X[1],lambda = param[i])
+    alpha_matrix [i,1] = delta[i] * do.call(emisf[[i]],c(x=X[1],param[i]))
+      
   }
   
   for (i in c(2:t)){
     for (j in c(1:m)){
-      alpha_matrix [j,i] =dpois(X[i],lambda = param[j])  *sum(alpha_matrix[,i-1] * trans[,j])
+      alpha_matrix [j,i] =do.call(emisf[[j]],c(x=X[i],param[j])) *sum(alpha_matrix[,i-1] * trans[,j])
       
     }
   }
   return(alpha_matrix)
 }
+
+
+
+
+X = rnorm(5)
+forward(hmm,X)
