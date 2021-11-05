d37897715091fc890106d255d23ce3f93ae219c0
Esben <esbenskipper@outlook.com>
2021-11-05 16:30:44 +0100

    Minor changes

--- R/forward.R
+++ R/forward.R
@@ -12,20 +12,12 @@ forward <- function(HM,X){
   alpha_matrix = matrix(c(0),m,t)
   for (i in c(1:m)){
     alpha_matrix [i,1] = delta[i] * do.call(emisf[[i]],c(list(x=X[1]),param[[i]]))
-      
   }
-  
   for (i in c(2:t)){
     for (j in c(1:m)){
       alpha_matrix [j,i] =do.call(emisf[[j]],c(list(x=X[i]),param[[j]])) *sum(alpha_matrix[,i-1] * trans[,j])
-      
     }
   }
   return(alpha_matrix)
 }
 
-
-
-
-
-forward(hmm,X)
