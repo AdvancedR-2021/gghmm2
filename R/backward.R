25194f619dd05ff0e9d44b45a1dfa2b9996d4a94
Esben <esbenskipper@outlook.com>
2021-11-05 16:29:41 +0100

    Generalized backward

--- R/backward.R
+++ R/backward.R
@@ -1,4 +1,4 @@
-#Temporary - needs generalisation of dpois
+#Temporary - needs generalization of dpois
 
 backward <- function(trans,X,param){
   m = dim(trans)[1]
@@ -6,8 +6,11 @@ backward <- function(trans,X,param){
   beta_matrix= matrix(1,m,t)
   for (i in rev(c(1:(t-1)))){
     for (j in c(1:m)){
-      beta_matrix[j,i] = sum(dpois(X[i+1],lambda = param) *  beta_matrix[,i+1] * trans[j,])
-      
+      empty_vec = c(1:m)
+      for (a in c(1:m)){
+        empty_vec[a] = do.call(emisf[[a]],c(x=x[i+1],param[[a]]))
+      }
+      beta_matrix[j,i] = sum(empty_vec*  beta_matrix[,i+1] * trans[j,])
     }
   }
   return( beta_matrix)
